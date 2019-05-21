7c44f87a723d08b412de69d83b1363cea57ae919
katodix <darko.katovic@gmail.com>
2019-05-21 22:14:28 +0200

    Major File Upload

---
+++ WebApps/WebApp.RazorPages/Pages/Somatotype/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.RazorPages.Pages.Somatotype
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            string sf_si_str = Request.Form["skinfold_suprailliac"];
+            // do something with emailAddress
+            double sf_si = double.Parse(sf_si_str);
+        }
+
+    }
+}
